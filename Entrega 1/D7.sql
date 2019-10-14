c1e4b015c6f497a3c6d1bf62c79c60d4968dacd3
Jony635 <jony635molina@gmail.com>
2019-10-14 20:32:20 +0200

    end

--- Entrega 1/D7.sql
+++ Entrega 1/D7.sql
@@ -3,11 +3,11 @@ select
 date(d.date) as "Date",
 -- count(distinct s.player_id) as "People",
 -- count(distinct case when s.player_id = _s.player_id and date(_s.start) != date(d.date) then s.player_id end) as "People_Returned",
-ifnull((count(distinct case when s.player_id = _s.player_id and date(_s.start) != date(d.date) then s.player_id end) / count(distinct s.player_id) * 100), 0) as "D1"
+ifnull((count(distinct case when s.player_id = _s.player_id and date(_s.start) != date(d.date) then s.player_id end) / count(distinct s.player_id) * 100), 0) as "D7"
 
 from dates as d
 left join sessions as s on date(d.date) = date(s.start)
-left join sessions as _s on date(_s.start) <= date(d.date) and date(_s.start) >= date(d.date) - interval 1 day
+left join sessions as _s on date(_s.start) <= date(d.date) and date(_s.start) >= date(d.date) - interval 7 day
 
 group by 1
 order by 1
--- Entrega 1/DAU_Day_Gender.sql
+++ Entrega 1/DAU_Day_Gender.sql
@@ -2,7 +2,7 @@ select
 date(d.date) as "Date",
 count(distinct case when u.sex = "M" then s.player_id end) as "DAU_Men",
 count(distinct case when u.sex = "F" then s.player_id end) as "DAU_Women"
-
+		
 from dates as d
 left join sessions as s on date(s.start) = date(d.date)
 left join users as u on u.user_id = s.player_id
